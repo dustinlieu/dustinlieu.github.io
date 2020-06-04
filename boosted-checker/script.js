ffe8802700aa4c7db14a2a32895287c37b30796b
dustinlieu <octylpy@gmail.com>
2020-06-03 21:59:29 -0700

    added license headers

--- boosted-checker/script.js
+++ boosted-checker/script.js
@@ -1,3 +1,21 @@
+/*
+	This file is part of the Boosted Board checker (https://github.com/dustinlieu/dustinlieu.github.io).
+	Copyright 2020 Dustin Lieu.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const boardModels = {
 	1: "Boosted Dual",
 	2: "Boosted Dual+",
@@ -19,7 +37,9 @@ const rideModes = {
 const batteryModels = {
 	0: "UNKNOWN 0",
 	1: "B2SR",
-	2: "B2XR"
+	2: "B2XR",
+	3: "UNKNOWN 3",
+	4: "UNKNOWN 4",
 };
 
 const valueElementIDs = [
